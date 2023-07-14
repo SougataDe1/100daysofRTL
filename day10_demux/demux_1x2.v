4f762d7cd41a22dad7ff86c5bff171b8233cc690
SOUGATA DE <136006783+SougataDe1@users.noreply.github.com>
2023-07-15 02:06:26 +0530

    Update demux_1x2.v

--- day10_demux/demux_1x2.v
+++ day10_demux/demux_1x2.v
@@ -5,14 +5,6 @@ output y0,y1,
 input s,
 input i
 );
-//assign {y0,y1} = s?{1'b0,i}: {i,1'b0};
-//    always @(*)
-//    begin
-//      if(s)
-//        {y[1],y[0]} = {1'b0,i};
-//      else
-//        {y[1],y[0]} = {1'b0,i};
-//    end
 assign y0=i & (~s);
 assign y1=i & (s);
 endmodule

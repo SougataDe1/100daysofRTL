8bc2f2297217fc47b36e167589e0b82cbed7f442
SOUGATA DE <136006783+SougataDe1@users.noreply.github.com>
2023-08-12 01:07:09 +0530

    Create parallel_in_parallel_out.v

---
+++ day31_pipo/parallel_in_parallel_out.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+module pipo(q,qb,din,clk,rst);
+output [3:0]q,qb;
+input [3:0]din;
+input clk;
+input rst;
+
+d_ff d1(q[0],qb[0],din[0],clk,rst);
+d_ff d2(q[1],qb[1],din[1],clk,rst);
+d_ff d3(q[2],qb[2],din[2],clk,rst);
+d_ff d4(q[3],qb[3],din[3],clk,rst);
+endmodule
+
+module d_ff(q,qb,din,clk,rst);
+output reg q;
+output reg qb;
+input din,clk,rst;
+always @(posedge clk)
+if(rst)
+begin
+q<=0;
+qb<=1;
+end
+else
+begin
+q<=din;
+qb<=~din;
+end
+endmodule
